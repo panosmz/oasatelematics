00398bd30c8d7e4f6b003ee50c4002520dc19399
panosmz <p.mazarakis94@gmail.com>
2019-03-07 19:08:51 +0200

    added dist config

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'oasatelematics',
+  packages = ['oasatelematics'],
+  version = '0.1',
+  license='gpl-3.0',
+  description = 'Python wrapper for the OASA Telematics API',
+  author = 'panosmz',
+  author_email = 'p.mazarakis94@gmail.com',
+  url = 'https://github.com/panosmz/oasatelematics',
+  download_url = 'https://github.com/panosmz/oasatelematics/archive/v0.1.tar.gz',
+  keywords = ['api', 'telematics'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
